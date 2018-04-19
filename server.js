dac2834ed5948205fc1bb6e2502f9f90f5d26c87
Flavio Araujo <flavioafa@gmail.com>
2018-04-19 14:12:44 -0300

    Atualização para deploy no heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+var path = require('path')
+
+const
+  express = require('express'),
+  serveStatic = require('serve-static'),
+  history = require('connect-history-api-fallback'),
+  port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(history())
+app.use(serveStatic(path.join() + '/dist/spa-mat'))
+app.listen(port)
